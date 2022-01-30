54b7f99dac6be8877aa07c2c3776e3c63ba55cc1
Rafael <rafael.uribe.2080@gmail.com>
2022-01-30 15:59:19 -0500

    Shop page style

---
+++ src/components/shop/ShopItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function ShopItem({ label, price, onClick, img }) {
+  return (
+    <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+      <div className="product__item">
+        <div className="product__item__pic set-bg">
+          <img src={img} alt="" />
+        </div>
+        <div className="product__item__text">
+          <h6>{label}</h6>
+          <button type="button" className="add-cart" onClick={onClick}>
+            + Buy
+          </button>
+          <h5>{price + ' $'}</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShopItem;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ShopItem from '../components/shop/ShopItem';
+
+import product1 from '../img/product/product-1.jpg';
+import product2 from '../img/product/product-2.jpg';
+import product3 from '../img/product/product-3.jpg';
+import product4 from '../img/product/product-4.jpg';
 
 function Shop() {
   return (
-    <div>
-      <h1>Shop</h1>
+    <div className="container">
+      <div className="row product__filter" style={{ marginTop: '15px' }}>
+        <ShopItem label="Shoes" price="100" onClick={() => {}} img={product1} />
+        <ShopItem label="Shoes" price="200" onClick={() => {}} img={product2} />
+        <ShopItem label="Shoes" price="150" onClick={() => {}} img={product3} />
+        <ShopItem label="Shoes" price="250" onClick={() => {}} img={product4} />
+      </div>
     </div>
   );
 }
