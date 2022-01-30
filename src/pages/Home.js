6329281702212539425ba2aab363cd8ee6632467
Rafael <rafael.uribe.2080@gmail.com>
2022-01-30 16:50:05 -0500

    Home Style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+const colorStyle = { color: '#f6dd69' };
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home__background">
+      <div className="container" style={{ marginTop: '30px' }}>
+        <h1>Our Smart Kitchen</h1>
+        <h1>Platform is</h1>
+        <h1 style={colorStyle}>go to the restaurant</h1>
+        <h1 style={colorStyle}>or all restaurants</h1>
+        <h1>in the future</h1>
+      </div>
     </div>
   );
 }
